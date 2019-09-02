df03d153576c5d8344b127f14dcaee14679d8929
Krunal Dodiya <kunal.dodiya1@gmail.com>
2019-09-02 20:40:35 +0530

    init

--- src/components/Posts/regular_image_post.tsx
+++ src/components/Posts/regular_image_post.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Dimensions, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {Dimensions, Image, Text, View} from 'react-native';
 import FastImage from 'react-native-fast-image';
 import getAssets from '../../libs/image';
 
@@ -12,23 +12,38 @@ const RegularImagePost = (props: RegularImagePostProps) => {
   const {item} = data;
 
   const {width} = Dimensions.get('window');
+  const [height, setHeight] = useState(width);
+
+  Image.getSize(
+    getAssets(item.owner.avatar),
+    (w, h) => {
+      setHeight((width / h) * w);
+    },
+    () => null
+  );
 
   return (
-    <View
-      style={{
-        width: width,
-        justifyContent: 'center',
-      }}>
-      <View style={{padding: 10}}>
-        <Text style={{fontSize: 16}}>{item.when}</Text>
+    <View style={{width, justifyContent: 'center'}}>
+      <View style={{flexDirection: 'row', padding: 10}}>
+        <View>
+          <Image style={{width: 40, height: 40}} source={{uri: getAssets(item.owner.avatar)}} />
+        </View>
+
+        <View style={{flex: 1}}>
+          <Text>{item.owner.name}</Text>
+          <Text>{item.when}</Text>
+        </View>
       </View>
-      <FastImage
-        style={{
-          width: width,
-          height: width,
-        }}
-        source={{uri: getAssets(item.url)}}
-      />
+
+      {item.description && (
+        <View style={{padding: 10}}>
+          <Text>{item.description}</Text>
+        </View>
+      )}
+
+      <View>
+        <FastImage style={{width, height}} source={{uri: getAssets(item.url)}} />
+      </View>
     </View>
   );
 };
